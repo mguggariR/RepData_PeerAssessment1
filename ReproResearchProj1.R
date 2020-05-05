98253b5dd96fd4f8c7c494391e8fe92f56d180b0
Mallik Guggari <mguggari@gmail.com>
2020-05-05 00:05:46 -0500

    Fully working with lots of extra code to be cleaned up

--- ReproResearchProj1.R
+++ ReproResearchProj1.R
@@ -55,14 +55,59 @@ library(reshape2)
                 facet_grid(variable~.)+
                 labs(title = "Histogram with and without filling missing data")        
                 
-## create mean and median of dayly number of steps
+## create mean and median of daily number of steps with modified dataset
         ModdailyMean <- mean(ModFinalDF$ModDailySteps)
         ModdailyMedian <- median(ModFinalDF$ModDailySteps)      
                          
+## *******************
+        myFunc <- function(x){
+                ifelse((weekdays(x, abbreviate=TRUE) == "Sat" | 
+                                weekdays(x, abbreviate=TRUE) == "Sun"), "WeekEnd", "WeekDay")
+                       }
                          
-                         
- 
- 
+## new Data Frame factored with week and weekend days
+        ModDayDF <- mutate(ModDF, Day=myFunc(date)) 
+        
+        myGrSumDF <- as.data.frame(ModDayDF %>% 
+                        select(date, Day, interval, ModSteps)%>% 
+                        group_by(Day, interval)%>% 
+                        summarise(AverageSteps=mean(ModSteps)))
+        
+        ggplot(data=myGrSumDF, aes(x=interval, y=AverageSteps, color=Day, group=Day)) + 
+                geom_line()+
+                labs(title="Average steps for each time interval")+
+                facet_grid(Day ~ .)
+                
         
         
+        ModWkendDF <- filter(ModDayDF, Day=="WeekEnd")                
+        ModWkdayDF <- filter(ModDayDF, Day=="WeekDay")
+        
+        
+        
+        myTestDF <- select(ModDayDF, date, Day, interval, ModSteps)
+        myTestDF1 <- filter(myTestDF, (interval=="0" | interval=="5" |interval=="10" | interval=="15"))
+        myGrTestDF <- myTestDF1 %>% group_by(Day, interval)
+        myGrSumDF <-  as.data.frame( summarise(myGrTestDF, intervalMean=mean(ModSteps)))
+        
+        stepsperday <- as.data.frame( myTestDF1 %>% group_by(Day) %>% group_by(interval)%>%
+                                              summarise(intervalMean=mean(ModSteps))) 
+ 
+## Calculate average number of steps for each time interval for both week days in a new data frame
+        WkStepsPerIntervalDF <- as.data.frame( ModWkdayDF %>% group_by(Day) %>% group_by(interval)%>%
+                                                     summarise(intervalMean=mean(ModSteps)))       
+## create a plot of average number of steps for each time interval        
+        ggplot(data=WkStepsPerIntervalDF, aes(x=interval, y=intervalMean))+ 
+                geom_line(color="steelblue")+
+                labs(title="Average number of steps for each time interval", ylab="Mean of steps")  
+        
+## Calculate average number of steps for each time interval for both weekend days in a new data frame
+        WkEndStepsPerIntervalDF <- as.data.frame( ModWkendDF %>% group_by(Day) %>% group_by(interval)%>%
+                                                       summarise(intervalMean=mean(ModSteps)))         
+        ggplot(data=WkEndStepsPerIntervalDF, aes(x=interval, y=intervalMean))+ 
+                geom_line(color="red")+
+                labs(title="Average number of steps for each time interval during weekend", ylab="Mean of steps")  
+        
+  testDF <-  ModDayDF  %>% group_by(Day) %>% group_by(interval)   
+          
         
